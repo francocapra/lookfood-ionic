91d66c2f62c4bc763ea7d96208705ef7ed56a581
Franco Capra <franco.capra@outlook.com>
2018-05-11 02:29:02 -0300

    Home Screen

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -9,8 +9,8 @@ import { NgModule } from '@angular/core';
 import { HomePage } from './home'; //IMPORTANTE!!! O nome "HomePage", DEVE estar com o MESMO nome da classe no CONTROLLER
  
 @NgModule({ 
-    declarations: [HomePage], //PÁGINA
-    imports: [IonicPageModule.forChild(HomePage)] //PADRÃO PARA SUBMÓDULO
+    declarations: [HomePage], //PÁGINA <VIEW> !!!   >>>> IMPORTANTE!!! O nome "HomePage", DEVE estar com o MESMO nome da classe no CONTROLLER
+    imports: [IonicPageModule.forChild(HomePage)] //PADRÃO PARA SUBMÓDULO  >>>>IMPORTANTE!!! O nome "HomePage", DEVE estar com o MESMO nome da classe no CONTROLLER
 }) 
 export class HomeModule { 
 } 
